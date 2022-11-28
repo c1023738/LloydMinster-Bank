b0cf2905beb697a5870a2709c30376bac684edc7
ETHAN BURGESS <c1023738@hallam.shu.ac.uk>
2022-11-28 09:53:33 +0000

    Added Login Form Design a Basic Pin Form Created the function when the buttons are Clicked

---
+++ LloydMinsterBank/LoginForm.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace LloydMinsterBank
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblAskUserText = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnEmpty2 = new System.Windows.Forms.Button();
+            this.btnPinNum0 = new System.Windows.Forms.Button();
+            this.btnEmpty1 = new System.Windows.Forms.Button();
+            this.btnPinNum9 = new System.Windows.Forms.Button();
+            this.btnPinNum8 = new System.Windows.Forms.Button();
+            this.btnPinNum7 = new System.Windows.Forms.Button();
+            this.btnPinNum3 = new System.Windows.Forms.Button();
+            this.btnPinNum5 = new System.Windows.Forms.Button();
+            this.btnPinNum4 = new System.Windows.Forms.Button();
+            this.btnPinNum2 = new System.Windows.Forms.Button();
+            this.btnPinNum6 = new System.Windows.Forms.Button();
+            this.btnPinNum1 = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 516);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(605, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 516);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblAskUserText
+            // 
+            this.lblAskUserText.AutoSize = true;
+            this.lblAskUserText.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAskUserText.Location = new System.Drawing.Point(266, 107);
+            this.lblAskUserText.Name = "lblAskUserText";
+            this.lblAskUserText.Size = new System.Drawing.Size(262, 38);
+            this.lblAskUserText.TabIndex = 2;
+            this.lblAskUserText.Text = "Please Enter Pin";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnEmpty2);
+            this.panel3.Controls.Add(this.btnPinNum0);
+            this.panel3.Controls.Add(this.btnEmpty1);
+            this.panel3.Controls.Add(this.btnPinNum9);
+            this.panel3.Controls.Add(this.btnPinNum8);
+            this.panel3.Controls.Add(this.btnPinNum7);
+            this.panel3.Controls.Add(this.btnPinNum3);
+            this.panel3.Controls.Add(this.btnPinNum5);
+            this.panel3.Controls.Add(this.btnPinNum4);
+            this.panel3.Controls.Add(this.btnPinNum2);
+            this.panel3.Controls.Add(this.btnPinNum6);
+            this.panel3.Controls.Add(this.btnPinNum1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(200, 220);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(405, 296);
+            this.panel3.TabIndex = 3;
+            // 
+            // btnEmpty2
+            // 
+            this.btnEmpty2.Location = new System.Drawing.Point(267, 222);
+            this.btnEmpty2.Name = "btnEmpty2";
+            this.btnEmpty2.Size = new System.Drawing.Size(135, 74);
+            this.btnEmpty2.TabIndex = 11;
+            this.btnEmpty2.UseVisualStyleBackColor = true;
+            // 
+            // btnPinNum0
+            // 
+            this.btnPinNum0.Location = new System.Drawing.Point(134, 222);
+            this.btnPinNum0.Name = "btnPinNum0";
+            this.btnPinNum0.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum0.TabIndex = 10;
+            this.btnPinNum0.Text = "0";
+            this.btnPinNum0.UseVisualStyleBackColor = true;
+            // 
+            // btnEmpty1
+            // 
+            this.btnEmpty1.Location = new System.Drawing.Point(0, 222);
+            this.btnEmpty1.Name = "btnEmpty1";
+            this.btnEmpty1.Size = new System.Drawing.Size(135, 74);
+            this.btnEmpty1.TabIndex = 9;
+            this.btnEmpty1.UseVisualStyleBackColor = true;
+            // 
+            // btnPinNum9
+            // 
+            this.btnPinNum9.Location = new System.Drawing.Point(267, 148);
+            this.btnPinNum9.Name = "btnPinNum9";
+            this.btnPinNum9.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum9.TabIndex = 8;
+            this.btnPinNum9.Text = "9";
+            this.btnPinNum9.UseVisualStyleBackColor = true;
+            this.btnPinNum9.Click += new System.EventHandler(this.btnPinNum9_Click);
+            // 
+            // btnPinNum8
+            // 
+            this.btnPinNum8.Location = new System.Drawing.Point(134, 148);
+            this.btnPinNum8.Name = "btnPinNum8";
+            this.btnPinNum8.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum8.TabIndex = 7;
+            this.btnPinNum8.Text = "8";
+            this.btnPinNum8.UseVisualStyleBackColor = true;
+            this.btnPinNum8.Click += new System.EventHandler(this.btnPinNum8_Click);
+            // 
+            // btnPinNum7
+            // 
+            this.btnPinNum7.Location = new System.Drawing.Point(0, 148);
+            this.btnPinNum7.Name = "btnPinNum7";
+            this.btnPinNum7.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum7.TabIndex = 6;
+            this.btnPinNum7.Text = "7";
+            this.btnPinNum7.UseVisualStyleBackColor = true;
+            this.btnPinNum7.Click += new System.EventHandler(this.btnPinNum7_Click);
+            // 
+            // btnPinNum3
+            // 
+            this.btnPinNum3.Location = new System.Drawing.Point(267, 0);
+            this.btnPinNum3.Name = "btnPinNum3";
+            this.btnPinNum3.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum3.TabIndex = 5;
+            this.btnPinNum3.Text = "3";
+            this.btnPinNum3.UseVisualStyleBackColor = true;
+            this.btnPinNum3.Click += new System.EventHandler(this.btnPinNum3_Click);
+            // 
+            // btnPinNum5
+            // 
+            this.btnPinNum5.Location = new System.Drawing.Point(134, 74);
+            this.btnPinNum5.Name = "btnPinNum5";
+            this.btnPinNum5.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum5.TabIndex = 4;
+            this.btnPinNum5.Text = "5";
+            this.btnPinNum5.UseVisualStyleBackColor = true;
+            this.btnPinNum5.Click += new System.EventHandler(this.btnPinNum5_Click);
+            // 
+            // btnPinNum4
+            // 
+            this.btnPinNum4.Location = new System.Drawing.Point(0, 74);
+            this.btnPinNum4.Name = "btnPinNum4";
+            this.btnPinNum4.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum4.TabIndex = 3;
+            this.btnPinNum4.Text = "4";
+            this.btnPinNum4.UseVisualStyleBackColor = true;
+            this.btnPinNum4.Click += new System.EventHandler(this.btnPinNum4_Click);
+            // 
+            // btnPinNum2
+            // 
+            this.btnPinNum2.Location = new System.Drawing.Point(134, 0);
+            this.btnPinNum2.Name = "btnPinNum2";
+            this.btnPinNum2.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum2.TabIndex = 2;
+            this.btnPinNum2.Text = "2";
+            this.btnPinNum2.UseVisualStyleBackColor = true;
+            this.btnPinNum2.Click += new System.EventHandler(this.btnPinNum2_Click);
+            // 
+            // btnPinNum6
+            // 
+            this.btnPinNum6.Location = new System.Drawing.Point(267, 74);
+            this.btnPinNum6.Name = "btnPinNum6";
+            this.btnPinNum6.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum6.TabIndex = 1;
+            this.btnPinNum6.Text = "6";
+            this.btnPinNum6.UseVisualStyleBackColor = true;
+            this.btnPinNum6.Click += new System.EventHandler(this.btnPinNum6_Click);
+            // 
+            // btnPinNum1
+            // 
+            this.btnPinNum1.Location = new System.Drawing.Point(0, 2);
+            this.btnPinNum1.Name = "btnPinNum1";
+            this.btnPinNum1.Size = new System.Drawing.Size(135, 74);
+            this.btnPinNum1.TabIndex = 0;
+            this.btnPinNum1.Text = "1";
+            this.btnPinNum1.UseVisualStyleBackColor = true;
+            this.btnPinNum1.Click += new System.EventHandler(this.btnPinNum1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 516);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.lblAskUserText);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblAskUserText;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnEmpty2;
+        private System.Windows.Forms.Button btnPinNum0;
+        private System.Windows.Forms.Button btnEmpty1;
+        private System.Windows.Forms.Button btnPinNum9;
+        private System.Windows.Forms.Button btnPinNum8;
+        private System.Windows.Forms.Button btnPinNum7;
+        private System.Windows.Forms.Button btnPinNum3;
+        private System.Windows.Forms.Button btnPinNum5;
+        private System.Windows.Forms.Button btnPinNum4;
+        private System.Windows.Forms.Button btnPinNum2;
+        private System.Windows.Forms.Button btnPinNum6;
+        private System.Windows.Forms.Button btnPinNum1;
+    }
+}
---
+++ LloydMinsterBank/LoginForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LloydMinsterBank
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPinNum1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPinNum9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
